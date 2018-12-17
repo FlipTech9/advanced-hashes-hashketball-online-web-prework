2c7f1d16817e64035f7d260632eb4825a2c4189a
Alex S <alex.sunga@fliptech.co>
2018-12-17 08:51:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,7 @@ def game_hash
       "Alan Anderson" => {
         :number => "0",
         :shoe => "16",
-        :points => "22",
+        :points => 22,
         :rebounds => "12",
         :assists => "12",
         :steals => "3",
